47003c00a3df52fb42685d6bb966ae6cc54177fc
Mohamed-125 <mohamedibrahim@gmail.com>
2025-02-04 20:02:13 +0200

    first commit

--- backend/api/index.js
+++ backend/api/index.js
@@ -14,24 +14,18 @@ const channelsRouter = require("../routes/channelsRouter");
 const noteRouter = require("../routes/noteRouter");
 const textRouter = require("../routes/textRouter");
 const cookieParser = require("cookie-parser");
-const puppeteer = require("puppeteer");
 
 // Mongo DB Connections
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
-app.use(express.json()); // To parse JSON data
-app.use(express.urlencoded({ extended: true })); // To parse URL-encoded data
-app.use(cookieParser()); // For parsing cookies
-
 mongoose
   .connect(process.env.MONGO_DB_URL)
   .then((response) => {
-    ("MongoDB Connection Succeeded.");
+    console.log("MongoDB Connection Succeeded.");
   })
   .catch((error) => {
-    "Error in DB connection: " + error;
+    console.log("Error in DB connection: " + error);
   });
+
+const whitelist = ["https://new-anki-one.vercel.app", "http://localhost:5173"];
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -74,5 +68,5 @@ app.use("/api/v1/text", textRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  "App running in port: " + PORT;
+  console.log("App running in port: " + PORT);
 });
