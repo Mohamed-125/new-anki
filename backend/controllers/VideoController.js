f3b71122b5b07b2d88fe787a7a38b5c47f59320c
Mohamed-125 <mohamedibrahim@gmail.com>
2025-02-27 15:48:24 +0200

    t

--- backend/controllers/VideoController.js
+++ backend/controllers/VideoController.js
@@ -98,13 +98,13 @@ const getTranscript = async (videoId, lang) => {
 };
 
 module.exports.getTranscript = async (req, res) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  // if (req.method === "OPTIONS") {
-  //   return res.status(200).end();
-  // }
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   const { videoId } = req.query;
 
