0082b642c88e7f787e93c15bf1241d995dbda82a
Mohamed-125 <mohamedibrahim.fed@gmail.com>
2026-05-02 00:10:15 +0300

    fix cors preflight r1

--- backend/api/index.js
+++ backend/api/index.js
@@ -68,7 +68,24 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,DELETE,PATCH,OPTIONS"
+  );
 
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 // app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json({ limit: "50mb" })); // Increase JSON body size limit
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
